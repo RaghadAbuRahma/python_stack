2c2c0e7bca1b0e3a4101cf5df3b5dbfebf63a2ac
Raghad Aburahma <raghad.sa201@gmail.com>
2024-04-28 16:48:20 +0300

    loops file

---
+++ _python/python_fundamentals/ForLoopsBasic1.py/for_loop_basic1.py
@@ -0,0 +1,37 @@
+
+# for x in range(0, 151, 1):
+#     print(x)
+
+
+
+# for x in range(1, 201, 1):
+#     print (x * 5)
+
+
+
+# for x in range(1, 101, 1):
+
+#     if x%10 == 0:
+#         print("Coding dojo")
+#     elif x%5 == 0: 
+#         print("Coding")
+
+
+
+# sum = 0
+# for x in range(0, 500000, 1):
+#     if(x%2 != 0): 
+#         sum = sum + x 
+# print(sum)
+
+
+# for x in range(2018, 0, -4):
+#     print(x)
+
+
+# lowNum = 2
+# highNum = 9
+# mult = 3
+# for x in range(lowNum, highNum+1, 1):
+#     if( x%mult == 0):
+#         print(x)
